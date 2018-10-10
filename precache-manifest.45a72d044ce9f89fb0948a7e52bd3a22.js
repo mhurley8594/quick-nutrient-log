51f464e7cc06d0a7ba8b2270af79b036423c2ce7
Michael Hurley <mike@swampsoftware.org>
2018-10-10 11:59:49 -0400

    Initial commit

---
+++ precache-manifest.45a72d044ce9f89fb0948a7e52bd3a22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f34b2211adbb9a090af3",
+    "url": "/static/js/main.f34b2211.chunk.js"
+  },
+  {
+    "revision": "a1ed6fe1fe7a3126d8b6",
+    "url": "/static/js/1.a1ed6fe1.chunk.js"
+  },
+  {
+    "revision": "f34b2211adbb9a090af3",
+    "url": "/static/css/main.71fd0577.chunk.css"
+  },
+  {
+    "revision": "1980f2d80b1c750593fab0c2825666eb",
+    "url": "/index.html"
+  }
+];
